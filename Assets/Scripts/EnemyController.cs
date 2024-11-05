9ca50b89b74eafa8c9afefb0d69a213205c44441
alejandrocy99 <moflycy@gmail.com>
2024-11-05 10:17:17 +0100

    enemigo y mapa

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,22 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public float velocidad;
+    private Vector3 posicionArriba;
+    public Vector3 posicionAbajo;
+    private bool subiendo;
+    // Puedes definir la distancia que quieres que el enemigo suba desde su posición inicial
+    public float distanciaSubida = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Posición inicial
+        Vector3 posicionInicial = transform.position;
+
+        // Establecemos la posición de subida en relación a la posición inicial
+        posicionArriba = posicionInicial + new Vector3(0, distanciaSubida, 0);
+
+        // La posición de bajada puede ser establecida desde el Inspector o aquí directamente
+        if (posicionAbajo == Vector3.zero) // Opcionalmente solo si no está definida en el inspector
+        {
+            posicionAbajo = posicionInicial;
+        }
+
+        // Empezamos moviendo hacia arriba
+        subiendo = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Determinamos hacia dónde moverse
+        Vector3 posicionDestino = subiendo ? posicionArriba : posicionAbajo;
+
+        // Movemos el objeto hacia la posición destino
+        transform.position = Vector3.MoveTowards(transform.position, posicionDestino, velocidad * Time.deltaTime);
+
+        // Cambiamos la dirección cuando se alcanza una de las posiciones límite
+        if (transform.position == posicionArriba)
+        {
+            subiendo = false;
+        }
+        else if (transform.position == posicionAbajo)
+        {
+            subiendo = true;
+        }
         
     }
 
-    private void MoverEnemigo(){
-        
+    private void OnCollisionEnter2D(Collision2D colision){
+        if(colision.gameObject.CompareTag("jugador")){
+            Debug.Log("1 vida menos");
+            
+            colision.gameObject.GetComponent<PlayerController>().FinDelJuego();
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float velocidad;
     public float fuerza;
     private float xLimite = 20f;
     private Rigidbody2D fisica;
     private SpriteRenderer orientacion;
+
     //public GameObject proyectil;
 
 
-    void Start(){
+    void Start()
+    {
         fisica = GetComponent<Rigidbody2D>();
         orientacion = GetComponent<SpriteRenderer>();
     }
@@ -22,15 +25,16 @@ public class NewBehaviourScript : MonoBehaviour
         // Movimiento horizontal del jugador
         float move = Input.GetAxis("Horizontal");
         //transform.Translate(move, 0f, 0f);
-        fisica.velocity = new Vector2(move * velocidad,fisica.velocity.y);
-        
-        if(Input.GetKeyDown(KeyCode.Space)){
-            
-            if(TocarSuelo()) fisica.AddForce(Vector2.up * fuerza,ForceMode2D.Impulse); 
+        fisica.velocity = new Vector2(move * velocidad, fisica.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            if (TocarSuelo()) fisica.AddForce(Vector2.up * fuerza, ForceMode2D.Impulse);
         }
-    //modifica el flix en funcion a donde mira
-        if (fisica.velocity.x < 0f)  orientacion.flipX = true;
-        else if(fisica.velocity.x > 0f) orientacion.flipX = false; 
+        //modifica el flix en funcion a donde mira
+        if (fisica.velocity.x < 0f) orientacion.flipX = true;
+        else if (fisica.velocity.x > 0f) orientacion.flipX = false;
         // Limitar el movimiento izquierda-derecha
         Vector3 posicion = transform.position;
         posicion.x = Mathf.Clamp(posicion.x, -xLimite, xLimite);
@@ -43,8 +47,15 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
 
-    private bool TocarSuelo(){
-        RaycastHit2D siTocA = Physics2D.Raycast(transform.position + new Vector3(0f,-2f,0f),Vector2.down,0.2f);
-         return siTocA.collider != null;
+    private bool TocarSuelo()
+    {
+        RaycastHit2D siTocA = Physics2D.Raycast(transform.position + new Vector3(0f, -2f, 0f), Vector2.down, 0.2f);
+        return siTocA.collider != null;
+    }
+
+    public void FinDelJuego()
+    {   
+        Time.timeScale = 0f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
