bc77f30b741963c7e19f32bce87e20c7e4dfac7a
alejandrocy99 <moflycy@gmail.com>
2024-11-19 01:04:36 +0100

    añadir puntuacion y mejoras

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField] private GameObject efecto;
+    [SerializeField] private float cantidadPuntos;
+    [SerializeField] private Puntuacion puntuacion;
+    
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Player")){
+            puntuacion.SumarPuntos(cantidadPuntos);
+            Instantiate(efecto,transform.position,Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 }
